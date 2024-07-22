2a3d1ccc2dee5e93fe66efd448a0c63e3fbf443b
Bogdan Petrukovich <viorbrint@gmail.com>
2024-07-22 22:54:49 +0300

    feat: play with expressjs

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,6 +5,7 @@ import { tracksRouter } from "./routers/tracks.router";
 const app = express();
 app.use("/api", globalRouter);
 app.use("/api/tracks", tracksRouter);
+
 app.listen(3000, () => {
   console.log("Server start");
 });
