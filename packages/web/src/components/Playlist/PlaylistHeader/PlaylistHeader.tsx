775162e6f630cb78661138182d1e502b4fac6768
Bogdan Petrukovich <viorbrint@gmail.com>
2024-08-07 18:48:14 +0300

    feat(ui): apply gradient to playlist header

--- packages/web/src/components/Playlist/PlaylistHeader/PlaylistHeader.tsx
+++ packages/web/src/components/Playlist/PlaylistHeader/PlaylistHeader.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 import React from 'react'
+import { Notable } from 'next/font/google'
+
+const epicFont = Notable({ weight: '400', subsets: ['latin'] })
 
 export enum PlaylistEnum { Playlist = 'Playlist', Album = 'Album' };
 
@@ -11,22 +14,24 @@ interface PlaylistHeaderProps {
 
 export default function PlaylistHeader({ type, name, author }: PlaylistHeaderProps) {
     return (
-        <div className='bg-gradient-to-b from-blue-700 to-blue-950'>
-            <div className='flex gap-5 p-8 items-end'>
-                <Image className='size-48 rounded-md shadow-black shadow-2xl' src='/test-imgs/to-pimp.jpg' alt='to pimp a butterfly' width={512} height={512} />
-                <div className='flex flex-col gap-2'>
-                    <div className='text-sm'>
-                        {type}
-                    </div>
-                    <div className='text-8xl'>
-                        {name}
+        <div className='bg-gradient-to-b from-blue-500 to-blue-950'>
+            <div className='flex gap-5 p-8 text-text'>
+                <Image className='size-48 rounded-md' src='/test-imgs/to-pimp.jpg' alt='to pimp a butterfly' width={512} height={512} />
+                <div className='flex flex-col justify-between'>
+                    <div className='flex flex-col gap-3'>
+                        <div className={`text-8xl ${epicFont.className}`}>
+                            {name}
+                        </div>
+                        <div>
+                            {type}
+                        </div>
                     </div>
                     <div>
                         {author}
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
