804d312a9306ea25e0e520b3ee22c43a268ba565
Bogdan Petrukovich <viorbrint@gmail.com>
2024-09-12 19:36:45 +0300

    fix(api): fix test endpoint

--- packages/server/src/router.ts
+++ packages/server/src/router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { tracksRouter } from "./core/tracks/tracksRouter";
+import { testRouter } from "./core/test/testRouter";
 
 export const router = Router();
 
 router.use("/tracks", tracksRouter);
+router.use("/test", testRouter);
